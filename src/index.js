ac082afc5a4638a8a099fa8db6717c3912dc8c89
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-14 15:07:26 +0300

    fix: bug for 535  and the same

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 module.exports = function reverse (n) {
- let resultString = "";
- if (n < 0) n = -1 * n;
- if (n % 10 == 0) n = n /10;
- while (n > 1) {
-     resultString += n % 10;
-     n = (n - n % 10) / 10;
-    if (n < 10) resultString += n;	
- }
-    return Number(resultString);
+    if (n < 0) n = -1 * n;
+    if (n % 10 == 0) n = n /10;
+    let arr =(n + "").split("");
+    console.log(arr);
+    let resultString = "";
+    
+    for (let i = arr.length-1; i > -1; i--) {
+        resultString += arr[i];
+    }
+       return Number(resultString);
 }

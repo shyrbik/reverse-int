60038c1ba4be98e01b5a9ec6d823a5d235391b64
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-14 14:39:43 +0300

    fix: n < 0 and n end for 0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = function reverse (n) {
  let resultString = "";
+ if (n < 0) n = -1 * n;
+ if (n % 10 == 0) n = n /10;
  while (n > 1) {
      resultString += n % 10;
      n = (n - n % 10) / 10;

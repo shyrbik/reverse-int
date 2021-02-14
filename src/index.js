43c8407868424806abb48006c419b2a5cab093c6
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-14 11:13:11 +0300

    feat: add my code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+ let resultString = "";
+ while (n > 1) {
+     resultString += n % 10;
+     n = (n - n % 10) / 10;
+    if (n < 10) resultString += n;	
+ }
+    return Number(resultString);
 }
